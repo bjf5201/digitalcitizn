da7c0f9d6d297922b9ceae5a80dc8cd6554c0347
Bethany Fannin <bjf5201@uncw.edu>
2023-08-07 17:38:51 -0400

    Rename landing page to landing-page

---
+++ src/templates/landing-page.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../components/Header'
+import ComingSoon from '../components/ComingSoon'
+
+const LandingPageTemplate = () => {
+return (
+  <div className="global-wrapper" data-is-root-path={isHomePage}>
+    <Header></Header>
+
+    <main>
+      <ComingSoon></ComingSoon>
+    </main>
+
+    <footer>
+      © {new Date().getFullYear()} {title}
+    </footer>
+  </div>
+)
+}
+
+export default LandingPageTemplate
