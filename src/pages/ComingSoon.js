794db1ed0fc5289fbf11d63dc9e471230c6ea3e4
Bethany Fannin <bjf5201@uncw.edu>
2023-08-08 20:59:08 -0400

    Update ComingSoon with new content

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { Link, graphql } from "gatsby"
 
-export default function ComingSoon() {
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const ComingSoon = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
   return (
-    <p>Coming Soon</p>
+    <Layout location={location} title={siteTitle}>
+      <Seo title="About Digital Citizn" />
+      <h1>About</h1>
+      <p>Information will go here</p>
+      <Link to="/">
+        <span role="link">Go back Home.</span>
+      </Link>
+    </Layout>
   )
 }
+
+export default ComingSoon
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
